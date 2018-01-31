d18e7c4ca949db2b8709c458e9433ac04a6cf167
ZabeRx <35974123+ZabeRx@users.noreply.github.com>
2018-01-30 21:01:15 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-   console.log("¡La pajarita está lista!");
-  
-   client.user.setGame('Experimentando con tu corazón.');
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
-  });
-  var prefix = config.prefix;
-  
-  client.on("message", (message) => {
-    if (message.content.startsWith(prefix + "ping")) {
-      message.channel.send("pong uwu");
-    }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
-  const palabras = [":v", "c mamo", ":'v", ":,v", "V:", ":y", ":u", "u:", "v:", "V:", ":V", ">:v", ">:V", "v:<", "V:<", "alv", "kha", "Ecks di"];
-    if(palabras.some(p => message.content.includes(p))){
-
-      message.delete( );
-    }
-  });
-  client.login(process.env.BOT_TOKEN);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);

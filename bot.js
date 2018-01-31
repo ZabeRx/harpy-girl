af15272239bed6130fca209d16b522e1e58c4690
ZabeRx <35974123+ZabeRx@users.noreply.github.com>
2018-01-30 21:09:01 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,13 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
-});
+    
+const palabras = [":v", "c mamo", ":'v", ":,v", "V:", ":y", ":u", "u:", "v:", "V:", ":V", ">:v", ">:V", "v:<", "V:<", "alv", "kha", "Ecks di"];
+    if(palabras.some(p => message.content.includes(p))){
+
+      message.delete( );
+    }
+  });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
